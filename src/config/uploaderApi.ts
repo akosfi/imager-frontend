33717b347cdfc3f6a2e95fb183d782dcbc9b3654
Akos Fi <fi.akos@straxus.hu>
2020-11-06 13:03:06 +0100

    deploy

--- src/config/uploaderApi.ts
+++ src/config/uploaderApi.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const uploaderApi = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
+    //Use this is hosted on different servers.
+    //baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
+    baseURL: `/api`,
     timeout: 1000,
     withCredentials: true
 });
