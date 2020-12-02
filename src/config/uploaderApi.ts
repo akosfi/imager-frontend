a22e00ce80c9015440fa157f198e84c2d4f5a3a2
Akos Fi <fi.akos@straxus.hu>
2020-12-02 19:22:28 +0100

    change

--- src/config/uploaderApi.ts
+++ src/config/uploaderApi.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const uploaderApi = axios.create({
-    //Use this is hosted on different servers.
-    //baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
-    baseURL: `/api`,
+    baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
     timeout: 1000,
     withCredentials: true
 });
